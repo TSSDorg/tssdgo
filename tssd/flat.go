b1d98a527b87612a10cfa86fb7d8fd0e5b45b9b6
BrookZhou <tssdorg@hotmail.com>
2026-07-05 10:45:13 +0000

    split factory.go from flat.go

--- tssd/flat.go
+++ tssd/flat.go
@@ -3,25 +3,21 @@ package tssd
 import (
 	"crypto/md5"
 	"encoding/hex"
-	"fmt"
-	"reflect"
 )
 
 var groups = map[string]*factory{}
 
 func Register(flat Flatable) {
-	g := flat.Group()
-	if _, ok := groups[g]; !ok {
-		factory := &factory{
+	group := flat.Group()
+	_, ok := groups[group]
+	if !ok {
+		groups[group] = &factory{
+			current:  flat.Version(), //register first one as the current
 			versions: make(map[string]*buildInfo, 0),
 			schemas:  make(map[string]*buildInfo, 0),
 		}
-		groups[g] = factory
-		factory.register(flat)
-		factory.current = flat.Version()
-		return
 	}
-	groups[g].register(flat)
+	groups[group].register(flat)
 }
 
 // default the first register one regard as current
@@ -32,26 +28,12 @@ func RegisterCurrent(flat Flatable) {
 }
 
 // return current version of the register group
+// return "" if group not exist
 func CurrentVersion(group string) string {
 	if factory, ok := groups[group]; ok {
 		return factory.current
 	}
 	return ""
-}
-
-type buildInfo struct {
-	version string //current version
-	progeny string //which version it can upgrade after decoration
-	schema  Schema //current schema
-	hash    string
-	info    *typeInfo
-	builder Flatable //keep it as builder
-}
-
-type factory struct {
-	current  string
-	versions map[string]*buildInfo //local we seek by names or version
-	schemas  map[string]*buildInfo //and remote we seek by schema
 }
 
 type Flatable interface {
@@ -93,8 +75,7 @@ type constrainFlatable[T any] interface {
 	*T
 }
 
-type Flat[T any, PT constrainFlatable[T]] struct {
-}
+type Flat[T any, PT constrainFlatable[T]] struct{}
 
 func (this *Flat[T, PT]) Build() Flatable {
 	return PT(new(T))
@@ -141,183 +122,30 @@ func (this *Flat[T, PT]) OnHeader(header Header) error {
 }
 
 func (this *Flat[T, PT]) Decorate(flat Flatable) Flatable {
-	return flat
-}
-
-func (factory *factory) register(flat Flatable) {
-	if factory.current == flat.Version() {
-		return
-	}
-
-	value := reflect.ValueOf(flat)
-	v := value.Type().Elem()
-
-	info := parse(reflect.New(v).Elem().Interface())
-	bi := &buildInfo{
-		version: flat.Version(),
-		progeny: flat.Progeny(),
-		info:    info,
-		builder: flat.Build(), //we build a new one, rather user's data
-	}
-
-	factory.versions[flat.Version()] = bi
-	bi.schema = flat.Schema()
-	hash := bi.schema.Hash
-	bi.hash = hash
-	factory.schemas[hash] = bi
-}
-
-func (factory *factory) Validate(header Header) error {
-	if header.Version != TSSD_VERSION {
-		return ErrorInvalidTSSDVersion
-	}
-	if _, ok := factory.schemas[header.Schema.Hash]; !ok {
-		return ErrorTSSDDataSchemaReject
-	}
-	return nil
-}
-
-func MarshalTo(flat Flatable, to []byte) ([]byte, error) {
-	if f, ok := groups[flat.Group()]; ok {
-		return f.marshalTo(flat, to)
-	}
-	return nil, ErrorTSSDDataUnregister
-}
-
-func (factory *factory) marshalTo(flat Flatable, dest []byte) ([]byte, error) {
-	bi, ok := factory.versions[flat.Version()]
-	if !ok {
-		return nil, ErrorTSSDDataSchemaReject
-	}
-
-	dest = appendHeader(dest, flat.Schema())
-	return bi.info.marshal(flat, dest)
+	return this
 }
 
 func Marshal(flat Flatable) ([]byte, error) {
 	return MarshalTo(flat, make([]byte, 0, 4096))
 }
 
-func (factory *factory) marshal(flat Flatable) ([]byte, error) {
-	return factory.marshalTo(flat, make([]byte, 0, 4096))
+func MarshalTo(flat Flatable, to []byte) ([]byte, error) {
+	if factory, ok := groups[flat.Group()]; ok {
+		return factory.marshalTo(flat, to)
+	}
+	return nil, ErrorTSSDDataUnregister
 }
 
 func UnmarshalTo(from []byte, to Flatable) (remain []byte, err error) {
-	g := to.Group()
-	if _, ok := groups[g]; !ok {
-		return nil, ErrorTSSDDataUnregister
+	if factory, ok := groups[to.Group()]; ok {
+		return factory.unmarshalTo(from, to)
 	}
-
-	return groups[g].unmarshalTo(from, to)
+	return nil, ErrorTSSDDataUnregister
 }
 
 func Unmarshal(from []byte, group string) (to Flatable, remain []byte, err error) {
-	if f, ok := groups[group]; ok {
-		return f.unmarshal(from)
+	if factory, ok := groups[group]; ok {
+		return factory.unmarshal(from)
 	}
-
 	return nil, nil, ErrorTSSDDataUnregister
-}
-
-// UnmarshalTo direct unmarshal to your object
-func (factory *factory) unmarshalTo(src []byte, dest Flatable) ([]byte, error) {
-	header, remain, err := dumpHeader(src)
-	if err != nil {
-		return src, err
-	}
-
-	if err := factory.Validate(*header); err != nil {
-		return src, err
-	}
-
-	if err := dest.OnHeader(*header); err != nil {
-		return src, err
-	}
-	remoteHash := header.Schema.Hash
-
-	//local := dest.Schema(factory)   //default it new objct, so we fetch it
-	local := factory.versions[dest.Version()].hash
-	bi, ok := factory.schemas[remoteHash]
-	if !ok {
-		fmt.Printf("local schema: %s doesn't match with remote: schema[%s] hash[%s]\n", local, header.Schema, remoteHash)
-		return src, ErrorTSSDDataSchemaReject
-	}
-
-	if local == remoteHash {
-		return bi.info.unmarshalTo(remain, dest)
-	}
-
-	obj := bi.builder.Build()
-	remain, err = bi.info.unmarshalTo(remain, obj)
-	if err != nil {
-		return src, err
-	}
-
-	_, err = factory.decorate(obj, dest)
-	if err != nil {
-		return src, err
-	}
-
-	return remain, nil
-}
-
-// chain upgate it to the latest
-func (factory *factory) decorate(flat, to Flatable) (Flatable, error) {
-	for v := flat.Progeny(); len(v) > 0; {
-		if v == to.Version() {
-			return to.Decorate(flat), nil
-		}
-		bi, ok := factory.versions[v]
-		if !ok {
-			fmt.Printf("local version %s not found\n", v)
-			return nil, ErrorTSSDDataSchemaReject
-		}
-		flat = bi.builder.Build().Decorate(flat)
-		v = flat.Progeny()
-	}
-	//your may specify unmarshal to a old one
-	return nil, ErrorTSSDDataSchemaReject
-}
-
-// Unmarshal we new a current version object for user and return the remain bytes after consum
-func (factory *factory) unmarshal(src []byte) (Flatable, []byte, error) {
-	header, remain, err := dumpHeader(src)
-	if err != nil {
-		return nil, src, err
-	}
-
-	if err := factory.Validate(*header); err != nil {
-		return nil, src, err
-	}
-
-	if err := factory.versions[factory.current].builder.OnHeader(*header); err != nil {
-		return nil, src, err
-	}
-	remoteHash := header.Schema.Hash
-
-	bi, ok := factory.schemas[remoteHash]
-	if !ok {
-		fmt.Printf("remote schema [%s] hash[%s] not found\n", header.Schema, remoteHash)
-		return nil, src, ErrorTSSDDataSchemaReject
-	}
-	obj := bi.builder.Build()
-
-	remain, err = bi.info.unmarshalTo(remain, obj)
-	if err != nil {
-		return nil, src, err
-	}
-
-	v := obj.Progeny()
-	if len(v) == 0 || obj.Version() == factory.current {
-		return obj, remain, nil
-	}
-
-	to := factory.versions[factory.current].builder.Build()
-
-	flat, err := factory.decorate(obj, to)
-	if err != nil {
-		return nil, src, err
-	}
-
-	return flat, remain, nil
 }
